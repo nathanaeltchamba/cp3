ab7c2a6a89ed89c3c984ae443e3a0822a5957930
nathanaeltchamba <natanshost@gmail.com>
2024-10-31 17:04:17 -0600

    add speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 
@@ -22,6 +23,7 @@ export default function RootLayout({
         <main className="relative overflow-hidden">
           {children}
           <Analytics />
+          <SpeedInsights />
         </main>
         <Footer />
       </body>
